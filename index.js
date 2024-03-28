a7eb60ad5107f60c482a528c1f91bb9b3b34a3b9
Rama Astra <ramaastra333@gmail.com>
2024-03-28 20:44:39 +0800

    feat: add morgan middleware

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
 const routerV1 = require('./routers/v1');
 const app = express();
 const port = 3000;
 
+app.use(morgan('dev'));
 app.use(express.json());
 
 app.use('/api/v1', routerV1);

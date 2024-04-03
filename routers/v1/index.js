56dd40b79af00d3873508036e862ee929b38eb31
Rama Astra <ramaastra333@gmail.com>
2024-04-03 23:44:47 +0800

    feat: add dummy auth middleware

--- routers/v1/index.js
+++ routers/v1/index.js
@@ -3,10 +3,11 @@ const router = Router();
 const userRouter = require('./user.router');
 const accountRouter = require('./account.router');
 const transactionRouter = require('./transaction.router');
+const auth = require('../../middlewares/auth.middleware');
 
-router.use('/users', userRouter);
-router.use('/accounts', accountRouter);
-router.use('/transactions', transactionRouter);
+router.use('/users', auth, userRouter);
+router.use('/accounts', auth, accountRouter);
+router.use('/transactions', auth, transactionRouter);
 
 router.get('/', (req, res) => {
   res.json({

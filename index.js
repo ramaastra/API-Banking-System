d4965814252b4cfded496530d7e4c18d51f471e5
Rama Astra <ramaastra333@gmail.com>
2024-04-03 22:09:26 +0800

    feat: setup cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const routerV1 = require('./routers/v1');
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
